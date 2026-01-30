91a5b3e8a98105d6d42c6ff24f5d95ae616f3933
Ramdev Murali <141579299+ramdevmurali@users.noreply.github.com>
2026-01-30 23:12:30 +0000

    infra/metrics: add z/ewma/vol/percentile columns

--- processor/src/db.py
+++ processor/src/db.py
@@ -44,6 +44,24 @@ async def init_tables():
                     vol_1m DOUBLE PRECISION,
                     vol_5m DOUBLE PRECISION,
                     vol_15m DOUBLE PRECISION,
+                    return_z_1m DOUBLE PRECISION,
+                    return_z_5m DOUBLE PRECISION,
+                    return_z_15m DOUBLE PRECISION,
+                    return_z_ewma_1m DOUBLE PRECISION,
+                    return_z_ewma_5m DOUBLE PRECISION,
+                    return_z_ewma_15m DOUBLE PRECISION,
+                    vol_z_1m DOUBLE PRECISION,
+                    vol_z_5m DOUBLE PRECISION,
+                    vol_z_15m DOUBLE PRECISION,
+                    vol_spike_1m BOOLEAN,
+                    vol_spike_5m BOOLEAN,
+                    vol_spike_15m BOOLEAN,
+                    p05_return_1m DOUBLE PRECISION,
+                    p05_return_5m DOUBLE PRECISION,
+                    p05_return_15m DOUBLE PRECISION,
+                    p95_return_1m DOUBLE PRECISION,
+                    p95_return_5m DOUBLE PRECISION,
+                    p95_return_15m DOUBLE PRECISION,
                     attention DOUBLE PRECISION,
                     PRIMARY KEY (time, symbol)
                 );
@@ -110,8 +128,19 @@ async def insert_metric(time, symbol, metrics: Dict[str, Any]):
     async with pool.acquire() as conn:
         await conn.execute(
             """
-            INSERT INTO metrics(time, symbol, return_1m, return_5m, return_15m, vol_1m, vol_5m, vol_15m, attention)
-            VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)
+            INSERT INTO metrics(
+                time, symbol,
+                return_1m, return_5m, return_15m,
+                vol_1m, vol_5m, vol_15m,
+                return_z_1m, return_z_5m, return_z_15m,
+                return_z_ewma_1m, return_z_ewma_5m, return_z_ewma_15m,
+                vol_z_1m, vol_z_5m, vol_z_15m,
+                vol_spike_1m, vol_spike_5m, vol_spike_15m,
+                p05_return_1m, p05_return_5m, p05_return_15m,
+                p95_return_1m, p95_return_5m, p95_return_15m,
+                attention
+            )
+            VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25)
             """,
             time,
             symbol,
@@ -121,6 +150,24 @@ async def insert_metric(time, symbol, metrics: Dict[str, Any]):
             metrics.get('vol_1m'),
             metrics.get('vol_5m'),
             metrics.get('vol_15m'),
+            metrics.get('return_z_1m'),
+            metrics.get('return_z_5m'),
+            metrics.get('return_z_15m'),
+            metrics.get('return_z_ewma_1m'),
+            metrics.get('return_z_ewma_5m'),
+            metrics.get('return_z_ewma_15m'),
+            metrics.get('vol_z_1m'),
+            metrics.get('vol_z_5m'),
+            metrics.get('vol_z_15m'),
+            metrics.get('vol_spike_1m'),
+            metrics.get('vol_spike_5m'),
+            metrics.get('vol_spike_15m'),
+            metrics.get('p05_return_1m'),
+            metrics.get('p05_return_5m'),
+            metrics.get('p05_return_15m'),
+            metrics.get('p95_return_1m'),
+            metrics.get('p95_return_5m'),
+            metrics.get('p95_return_15m'),
             metrics.get('attention'),
         )
 

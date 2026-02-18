76cebc4f36b25a7db96585b6e0cc7786fd49692f
Ramdev Murali <141579299+ramdevmurali@users.noreply.github.com>
2026-02-18 14:31:57 +0530

    tests: use migrate_db in integration fixtures

--- processor/tests/integration/conftest.py
+++ processor/tests/integration/conftest.py
@@ -13,7 +13,8 @@ import pytest_asyncio
 from aiokafka import AIOKafkaProducer
 
 from processor.src import config as config_module
-from processor.src.io.db import init_tables, close_pool, init_pool
+from processor.src.io.db import close_pool, init_pool
+from scripts import migrate_db
 
 
 def _compose_file() -> Path:
@@ -71,7 +72,7 @@ async def integration_services():
     await _wait_for_db(db_url)
     await _wait_for_kafka(brokers)
     await init_pool()
-    await init_tables()
+    await migrate_db.migrate()
     await close_pool()
     yield
 
